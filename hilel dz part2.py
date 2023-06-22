4959279cb27ee2b8e240193d54fbb64ab8f47d94
bmwlover <86006196+timkavenki@users.noreply.github.com>
2023-06-23 01:27:27 +0300

    Add files via upload

---
+++ hilel dz part2.py
@@ -0,0 +1,17 @@
+from art import *
+
+name_art = text2art("Timur", font='starwars')
+print(name_art)
+print("Escape sequences")
+print("\n\\a Bell (alert)")
+print("\\b Backspace")
+print("\\n New line")
+print("Horizontal tab: \tThis is a tab.")
+print("\\\\ Backslash \\")
+print("\\\" Double quotation mark\"")
+print("\\' Single quotation mark '")
+
+
+
+
+
---
+++ hilel dz.py
@@ -0,0 +1,4 @@
+import this
+import antigravity
+import __hello__
+from __future__ import braces
