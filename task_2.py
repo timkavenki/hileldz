1f21811e384cae1361a68b8563c6cf01b73f76fa
bmwlover <86006196+timkavenki@users.noreply.github.com>
2023-09-09 18:48:30 +0300

    Add files via upload

--- task_2.py
+++ task_2.py
@@ -1,13 +1,42 @@
-def is_palindrome(text):
-    # Видаляємо з тексту всі неалфавітні символи та перетворюємо на нижній регістр
-    cleaned_text = ''.join(filter(str.isalpha, text)).lower()
+import random
+from tabulate import tabulate
 
-    # Порівнюємо текст з його реверсом
-    return cleaned_text == cleaned_text[::-1]
+# Клас для представлення будинку
+class House:
+    def __init__(self):
+        self.population = random.randint(1, 100)
 
+# Клас для представлення вулиці з будинками
+class Street:
+    def __init__(self, number):
+        self.number = number
+        self.houses = [House() for _ in range(random.randint(5, 20))]
 
-text = input("Введіть текст: ")
-if is_palindrome(text):
-    print("Це паліндром!")
-else:
-    print("Це не паліндром.")
+# Клас для представлення міста
+class City:
+    def __init__(self, name):
+        self.name = name
+        self.streets = [Street(i + 1) for i in range(random.randint(2, 5))]
+
+    def get_population(self):
+        return sum(house.population for street in self.streets for house in street.houses)
+
+    def print_city_table(self):
+        table_data = []
+        for street in self.streets:
+            for i, house in enumerate(street.houses, start=1):
+                table_data.append([street.number, i, house.population])
+
+        table_headers = ["Вулиця", "Будинок", "Населення"]
+        print(tabulate(table_data, headers=table_headers))
+
+# Створення міста і його заповнення будинками
+my_city = City("Моє місто")
+
+# Отримання та виведення загальної кількості населення міста
+population = my_city.get_population()
+print(f"Загальна кількість населення у місті {my_city.name}: {population} осіб")
+
+# Виведення таблиці міста
+print("\nТаблиця міста:")
+my_city.print_city_table()
